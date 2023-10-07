e5a82409c3d0385d7e245aaf3699b4475dd12740
Kelly Silva <kellynha756@gmail.com>
2023-10-08 01:19:31 +0300

    add new scene

---
+++ Assets/Scripts/Staff.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour
+{
+    public float offset = 90f;
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 objectScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector3 direction = mousePosition - objectScreenPosition;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + offset;  
+
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(currentRotation.x, angle, currentRotation.z);
+    }
+
+}
